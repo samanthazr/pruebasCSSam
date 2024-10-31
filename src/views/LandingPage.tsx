1e3e29bf5f53b79f3e54521937502f4d92029736
samanthazr <samantha.zambrana@racquetsapp.com>
2024-10-31 18:50:42 -0400

    Add initial scaffolding

---
+++ src/views/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <h2 className='title'> Landing page </h2>
+    </>
+  );
+};
+
+export default LandingPage;
